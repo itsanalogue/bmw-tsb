163c334acb4f819a532669d2b7ddf96b8c1ae082
itsanalogue <139643481+itsanalogue@users.noreply.github.com>
2025-08-22 12:19:48 -0500

    add retries for network issues with forum posting

--- src/forum-update.ts
+++ src/forum-update.ts
@@ -100,6 +100,71 @@ export const encodeContentForVbulletin = (s: string) => {
   return out.replace(/%20/g, '+');
 };
 
+async function updatePost({
+  bbuserid,
+  bbpassword,
+  content,
+  post,
+}: {
+  bbuserid: string;
+  bbpassword: string;
+  content: string;
+  post: ForumPost;
+}) {
+  const showUrl = `https://${post.forumDomain}/forums/showpost.php?p=${post.postId}`;
+  const editUrl = `https://${post.forumDomain}/forums/editpost.php?do=editpost&p=${post.postId}`;
+  const updateUrl = `https://${post.forumDomain}/forums/editpost.php?do=updatepost&p=${post.postId}`;
+
+  const editPageRes = await fetch(editUrl, {
+    headers: {
+      'User-Agent': 'itsanalogue-bmw-tsb-updates',
+      Cookie: `bbuserid=${bbuserid}; bbpassword=${bbpassword}`,
+      Referer: showUrl,
+    },
+  });
+  if (!editPageRes.ok) {
+    throw new Error(
+      `Failed to load edit page for post ${post.postId} on ${post.forumDomain}: ${editPageRes.status} ${editPageRes.statusText}`,
+    );
+  }
+  const editPageText = await editPageRes.text();
+
+  const vbVersion = /vBulletin ([\d\.]+)/.exec(editPageText);
+  if (!vbVersion || vbVersion[1] !== VBULLETIN_VERSION) {
+    throw new Error(
+      `Unexpected vBulletin version (${vbVersion?.[1]}).  Will not attempt forum updates.`,
+    );
+  }
+
+  const csrfTokenMatch = /var\sSECURITYTOKEN\s=\s"([^"]+)";/.exec(editPageText);
+  if (!csrfTokenMatch) {
+    throw new Error(
+      `Failed to read CSRF token for ${post.postId} on ${post.forumDomain}`,
+    );
+  }
+  const csrfToken = csrfTokenMatch[1];
+
+  const encodedContent = encodeContentForVbulletin(content);
+
+  const postBody = `reason=&title=&message=${encodedContent}&wysiwyg=0&iconid=0&s=&securitytoken=${csrfToken}&do=updatepost&p=${post.postId}&sbutton=Save+Changes&parseurl=1&emailupdate=1`;
+
+  const updatePageRes = await fetch(updateUrl, {
+    method: 'POST',
+    headers: {
+      'User-Agent': 'itsanalogue-bmw-tsb-updates',
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Cookie: `bbuserid=${bbuserid}; bbpassword=${bbpassword}`,
+      Referer: editUrl,
+    },
+    body: postBody,
+  });
+  if (!updatePageRes.ok) {
+    throw new Error(
+      `Failed to post to ${post.postId} on ${post.forumDomain}: ${editPageRes.status} ${editPageRes.statusText}`,
+    );
+  }
+}
+
 export async function updateForumPosts(dataStore: TsbDataStore) {
   const bbpassword = process.env.BBPASSWORD;
   const bbuserid = process.env.BBUSERID;
@@ -112,6 +177,8 @@ export async function updateForumPosts(dataStore: TsbDataStore) {
     decodeURI(new URL(`../out/`, import.meta.url).pathname),
   );
   let updateCount = 0;
+  let retries = 3;
+
   for (const post of FORUM_POSTS) {
     const contentFilePath = path.join(outputPath, post.contentPath);
 
@@ -127,67 +194,40 @@ export async function updateForumPosts(dataStore: TsbDataStore) {
         continue;
       }
 
-      const showUrl = `https://${post.forumDomain}/forums/showpost.php?p=${post.postId}`;
-      const editUrl = `https://${post.forumDomain}/forums/editpost.php?do=editpost&p=${post.postId}`;
-      const updateUrl = `https://${post.forumDomain}/forums/editpost.php?do=updatepost&p=${post.postId}`;
-
-      const editPageRes = await fetch(editUrl, {
-        headers: {
-          'User-Agent': 'itsanalogue-bmw-tsb-updates',
-          Cookie: `bbuserid=${bbuserid}; bbpassword=${bbpassword}`,
-          Referer: showUrl,
-        },
-      });
-      if (!editPageRes.ok) {
-        throw new Error(
-          `Failed to load edit page for post ${post.postId} on ${post.forumDomain}: ${editPageRes.status} ${editPageRes.statusText}`,
-        );
+      while (retries > 0) {
+        try {
+          await updatePost({ bbpassword, bbuserid, content, post });
+          updateCount++;
+          // eslint-disable-next-line require-atomic-updates
+          dataStore.forumPostHashes[post.contentPath] = contentHash;
+          log.info(
+            `Updated forum post ${post.postId} on ${post.forumDomain} with ${post.contentPath}`,
+          );
+          break;
+        } catch (error) {
+          let canRetry = false;
+          const errorWithCause = error as Error & {
+            cause?: { message: string; code?: string };
+          };
+          switch (errorWithCause.cause?.code) {
+            case 'UND_ERR_CLOSED':
+            case 'UND_ERR_CONNECT_TIMEOUT':
+            case 'UND_ERR_SOCKET':
+              canRetry = true;
+              break;
+          }
+          if (canRetry && retries-- > 0) {
+            log.error(
+              `Retrying failed forum post update (${retries} retries remain)`,
+              errorWithCause.cause ?? error,
+            );
+            //delay 15s on retries
+            await new Promise((res) => global.setTimeout(res, 15000));
+            continue;
+          }
+          throw error;
+        }
       }
-      const editPageText = await editPageRes.text();
-
-      const vbVersion = /vBulletin ([\d\.]+)/.exec(editPageText);
-      if (!vbVersion || vbVersion[1] !== VBULLETIN_VERSION) {
-        throw new Error(
-          `Unexpected vBulletin version (${vbVersion?.[1]}).  Will not attempt forum updates.`,
-        );
-      }
-
-      const csrfTokenMatch = /var\sSECURITYTOKEN\s=\s"([^"]+)";/.exec(
-        editPageText,
-      );
-      if (!csrfTokenMatch) {
-        throw new Error(
-          `Failed to read CSRF token for ${post.postId} on ${post.forumDomain}`,
-        );
-      }
-      const csrfToken = csrfTokenMatch[1];
-
-      const encodedContent = encodeContentForVbulletin(content);
-
-      const postBody = `reason=&title=&message=${encodedContent}&wysiwyg=0&iconid=0&s=&securitytoken=${csrfToken}&do=updatepost&p=${post.postId}&sbutton=Save+Changes&parseurl=1&emailupdate=1`;
-
-      const updatePageRes = await fetch(updateUrl, {
-        method: 'POST',
-        headers: {
-          'User-Agent': 'itsanalogue-bmw-tsb-updates',
-          'Content-Type': 'application/x-www-form-urlencoded',
-          Cookie: `bbuserid=${bbuserid}; bbpassword=${bbpassword}`,
-          Referer: editUrl,
-        },
-        body: postBody,
-      });
-      if (!updatePageRes.ok) {
-        throw new Error(
-          `Failed to post to ${post.postId} on ${post.forumDomain}: ${editPageRes.status} ${editPageRes.statusText}`,
-        );
-      }
-
-      updateCount++;
-      // eslint-disable-next-line require-atomic-updates
-      dataStore.forumPostHashes[post.contentPath] = contentHash;
-      log.info(
-        `Updated forum post ${post.postId} on ${post.forumDomain} with ${post.contentPath}`,
-      );
     }
   }
   return updateCount;
